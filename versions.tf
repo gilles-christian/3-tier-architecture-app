f9f14f4d419c848b045053c1f34edf48e658abee
gilles-christian <mathias762001-2@yahoo.fr>
2022-09-19 15:57:50 +0100

    update version
    ok version provider

--- versions.tf
+++ versions.tf
@@ -1,11 +1,7 @@
 terraform {
   required_version = ">= 0.15"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28"
-    }
-    cloudinit = {
+      cloudinit = {
       source  = "hashicorp/cloudinit"
       version = "~> 2.1"
     }
